4b85a8d8cba9706d2581f07ca4fc60cd58aeab15
ben10hub <colinsbenny@gmail.com>
2021-11-04 13:47:09 +0530

    first commit

---
+++ MoviesAPI/Migrations/20211103051519_Moviecolections.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MoviesAPI.Migrations
+{
+    public partial class Moviecolections : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoviesGenres");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropColumn(
+                name: "Poster",
+                table: "Movies");
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailID",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "People",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoleID",
+                table: "People",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Role",
+                columns: table => new
+                {
+                    RoleID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rolename = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Role", x => x.RoleID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_People_RoleID",
+                table: "People",
+                column: "RoleID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_People_Role_RoleID",
+                table: "People",
+                column: "RoleID",
+                principalTable: "Role",
+                principalColumn: "RoleID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_People_Role_RoleID",
+                table: "People");
+
+            migrationBuilder.DropTable(
+                name: "Role");
+
+            migrationBuilder.DropIndex(
+                name: "IX_People_RoleID",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "EmailID",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "People");
+
+            migrationBuilder.DropColumn(
+                name: "RoleID",
+                table: "People");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Poster",
+                table: "Movies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MoviesGenres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(type: "int", nullable: false),
+                    MovieId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoviesGenres", x => new { x.GenreId, x.MovieId });
+                    table.ForeignKey(
+                        name: "FK_MoviesGenres_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MoviesGenres_Movies_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 4, "Adventure" },
+                    { 5, "Animation" },
+                    { 6, "Drama" },
+                    { 7, "Romance" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "MoviesGenres",
+                columns: new[] { "GenreId", "MovieId" },
+                values: new object[,]
+                {
+                    { 4, 2 },
+                    { 4, 3 },
+                    { 4, 4 },
+                    { 6, 2 },
+                    { 6, 3 },
+                    { 6, 5 },
+                    { 6, 6 },
+                    { 7, 5 },
+                    { 7, 6 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoviesGenres_MovieId",
+                table: "MoviesGenres",
+                column: "MovieId");
+        }
+    }
+}
